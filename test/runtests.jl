bfe820f306a443edafbe652f1dec9e8ff887cd14
Timo Kluck <tkluck@infty.nl>
2020-01-20 13:25:15 +0100

    Add test-cases for broadcast syntax

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using Test
 using InPlace
 
 @testset "in-place operations" begin
-
     a = big"1"
     b = a
     c = deepcopy(a)
@@ -22,4 +21,22 @@ using InPlace
     @inplace a -= 1
     @test a == b == 1
     @test c == 1
+
+    d = big"3"
+    @inplace a = d
+    @test a == b == d
+    @test c == 1
+
+    A = BigInt[1, 1, 1]
+    b = A[1]
+    c= deepcopy(A[1])
+
+    @inplace A .+= 1
+    @test A[1] == b == 2
+    @test c == 1
+
+    @inplace A .= 5
+    @test A[1] == b == 5
+    @test c == 1
+
 end
